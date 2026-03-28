e25f5854284cdd51332885d0aa4f2d99693fc777
Leandro Costa Magalhães Silva <leandrodevbackend@gmail.com>
2026-03-28 14:09:59 -0400

    Initial commit

---
+++ src/main/java/com/aeroambiental/sensoriamento/SensoriamentoApplication.java
@@ -0,0 +1,13 @@
+package com.aeroambiental.sensoriamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensoriamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensoriamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aeroambiental/sensoriamento/SensoriamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeroambiental.sensoriamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensoriamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
